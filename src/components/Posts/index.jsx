feba4fac1ca5f11d525dcdb30b73480dc04c0dbe
SivaMC <contatomiriancamila@gmail>
2021-04-02 10:16:10 -0300

    Adicionando o projeto

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,19 @@
+import { PostCard } from "../PostCard";
+import './styles.css';
+
+export const Posts = ({posts}) =>{
+    return(
+        <div className="posts">
+          {posts.map(post => (
+            <PostCard
+              /*id={post.id}
+              title={post.title}
+              body={post.body}
+              cover={post.cover}*/
+              key={post.id}
+              post={post}
+            />
+          ))}
+        </div>
+    );
+}
